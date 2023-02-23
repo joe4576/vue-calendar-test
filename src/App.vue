e5734e2fc5349fd2912eb60af2ff4888882f9033
Joe Brown <joebrown.4576@gmail.com>
2023-02-23 14:55:35 +0000

    Init Vue and Vuetify

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <v-row>
+    <v-col cols="12" sm="6">
+      <h1>Calendar Test</h1>
+    </v-col>
+  </v-row>
+</template>
